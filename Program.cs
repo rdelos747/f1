53be29689712f004ae437f3d23c8c407c35f65e0
raf <rdelos747@gmail.com>
2024-05-24 15:56:58 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Tesseract;
+
+namespace f1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+        string imgPath = "./images/fl.png";
+        string tessPath = @"./tessdata";
+        // using (var engine = new TesseractEngine(tessPath, "eng", EngineMode.Default))
+        // {
+
+        // }
+        using var engine = new TesseractEngine(tessPath, "eng", EngineMode.Default);
+        //using var img = Pix.LoadFromFile(imgPath);
+        //using var page = engine.Process(img);
+        //var text = page.GetText();
+
+        //Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence());
+        //Console.WriteLine("Text (GetText): \r\n{0}", text);
+    }
+}
